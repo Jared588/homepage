88bf3ac0ba09a25585a8e7ce6c63c70975af1ef6
Jared588 <jaredeprice@gmail.com>
2024-01-16 20:12:54 +0200

    Remove placeholder HTML for projects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import addProject from './project';
 
-const tImg = document.createElement('img');
-tImg.src = '../src/images/blank-profile.png';
+const tempImg = document.createElement('img');
+tempImg.src = '../src/images/blank-profile.png';
 
-addProject('Project 2', tImg, 'yayayaya');
+addProject('Project', tempImg, 'lalalala');
